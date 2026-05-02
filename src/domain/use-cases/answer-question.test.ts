3de4dbad2db2d8deadd74250e64c0dda869d6199
Giovani <giovani98@proton.me>
2026-05-02 09:51:03 -0300

    chore: install biome

--- src/domain/use-cases/answer-question.test.ts
+++ src/domain/use-cases/answer-question.test.ts
@@ -1,23 +1,23 @@
-import { expect, it } from "vitest";
-import type { Answer } from "../entities/answer.js";
-import type { AnswersRepository } from "../repositories/answers.repository.js";
-import { AnswerQuestionUseCase } from "./answer-question.js";
+import { expect, it } from 'vitest'
+import type { Answer } from '../entities/answer.js'
+import type { AnswersRepository } from '../repositories/answers.repository.js'
+import { AnswerQuestionUseCase } from './answer-question.js'
 
 const fakeAnswersRepository: AnswersRepository = {
-	create: async (answer: Answer) => {
-		return;
-	},
-};
+  create: async (_answer: Answer) => {
+    return
+  },
+}
 
-it("should create an answer", async () => {
-	const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository);
+it('should create an answer', async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
 
-	const answer = await answerQuestion.execute({
-		instructorId: "1",
-		questionId: "1",
-		content: "Nova resposta",
-	});
+  const answer = await answerQuestion.execute({
+    instructorId: '1',
+    questionId: '1',
+    content: 'Nova resposta',
+  })
 
-	expect(answer.id).toBeDefined();
-	expect(answer.content).toBe("Nova resposta");
-});
+  expect(answer.id).toBeDefined()
+  expect(answer.content).toBe('Nova resposta')
+})
