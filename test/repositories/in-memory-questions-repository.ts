c99e6fc644280e0ab0add81cf96d22b4216371a4
Giovani <giovani98@proton.me>
2026-05-05 21:36:47 -0300

    feat: add fetch recent questions use case + tests

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,3 +1,4 @@
+import type { PaginationParams } from '@/domain/forum/application/repositories/pagination-parameters.js'
 import type { QuestionsRepository } from '@/domain/forum/application/repositories/question.repository.js'
 import type { Question } from '@/domain/forum/enterprise/entities/question.js'
 
@@ -12,6 +13,14 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     }
 
     return question
+  }
+
+  async findManyRecent({ page }: PaginationParams) {
+    const questions = this.items
+      .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+      .slice((page - 1) * 20, page * 20)
+
+    return questions
   }
 
   async create(question: Question) {
